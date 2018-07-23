279aa15c4726ce7c4a524875271a59ff9562c887
Karim <6merkl@informatik.uni-hamburg.de>
2018-07-23 21:23:43 +0200

    print last exam

--- stine.py
+++ stine.py
@@ -14,6 +14,18 @@ def readAccountData():
 	f.close()
 	return (name, passwort)
 
+def deuglyfy(string):
+	index = string.find("<br>")
+	string = string[:index]
+	modulname = string.strip().replace("&nbsp;", "").replace("\t", "").replace("\n", "")
+	return modulname
+
+def handleChange(entry):
+	content = entry.find_element_by_tag_name('td').get_attribute('innerHTML')
+	modulname = deuglyfy(content)
+	print(modulname)
+
+entrynumber = -1
 account = readAccountData()
 options = Options()
 #options.add_argument("--headless")
@@ -26,11 +38,16 @@ driver.find_element_by_id("logIn_btn").click()
 driver.find_element_by_partial_link_text("Studium").click()
 driver.find_element_by_partial_link_text("Prüfungsergebnisse").click()
 
+entries = driver.find_elements_by_class_name("tbdata")
+entrynumber = len(entries)
 
 while True:
+	
 	driver.refresh()
-	entries = driver.find_element_by_class_name("tbdata")
+	entries = driver.find_elements_by_class_name("tbdata")
+	newnumber = len(entries)
+	# for testing always true
+	if entrynumber != len(entries) or True:
+		handleChange(entries[entrynumber-1])
 	time.sleep(300)
-
-
 		
