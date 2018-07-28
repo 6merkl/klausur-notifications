ce950d51fda601256d6589d9278b0649a4746784
Karim <6merkl@informatik.uni-hamburg.de>
2018-07-28 22:13:03 +0200

    use telegram as notification

--- notification.py
+++ notification.py
@@ -1,25 +1,7 @@
-import http.client, urllib
-
-def readApplicationData():
-	f = open("app.token", "r")
-	app = f.read()
-	f.close()
-	
-	f = open("user.token", "r")
-	user = f.read()
-	f.close()
-	return (app, user)
-app = readApplicationData()
+import subprocess
 
 def sendMessage(message):
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-      urllib.parse.urlencode({
-        "token": app[0],
-        "user": app[1],
-        "message": message,
-      }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
+    subprocess.call(["telegram-send", message])
 
 if __name__ == "__main__":
     sendMessage("Hallo Welt!")
