6b99463a0007c8c7687b5918178d39b59809c684
Karim <6merkl@informatik.uni-hamburg.de>
2018-08-01 01:03:50 +0200

    implementation using mechanicalsoup passing test but failing if used with real stine

--- stine.py
+++ stine.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import os
 import urllib.request
 import time
 import notification as notify
+from urllib.request import url2pathname
+import mechanicalsoup
+from bs4 import BeautifulSoup
 
 def readAccountData():
 	f = open("name.stine", "r")
@@ -16,39 +17,48 @@ def readAccountData():
 	return (name, passwort)
 
 def deuglyfy(string):
-	index = string.find("<br>")
+	index = string.find("<br")
 	string = string[:index]
 	modulname = string.strip().replace("&nbsp;", "").replace("\t", "")
 	modulname = modulname.replace("\n", "").replace("  ","")
 	return modulname
 
 def handleChange(entry):
-	content = entry.find_element_by_tag_name('td').get_attribute('innerHTML')
-	modulname = deuglyfy(content)
+	modulname = deuglyfy(entry)
 	message = modulname +" ist jetzt online!"
 	notify.sendMessage(message)
 
+def getEntries(browser):
+	soup = browser.get_current_page()
+	trs = soup.find_all("tr", class_ = "tbdata")
+	# remove <tr> by searching for td and "<td>\n" by slicing
+	return [str(x.find("td"))[5:] for x in trs]
+
 entrynumber = -1
 account = readAccountData()
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(chrome_options=options)
-driver.get("https://www.stine.uni-hamburg.de/")
 
-driver.find_element_by_name("usrname").send_keys(account[0])
-driver.find_element_by_name("pass").send_keys(account[1])
-driver.find_element_by_id("logIn_btn").click()
-driver.find_element_by_partial_link_text("Studium").click()
-driver.find_element_by_partial_link_text("Prüfungsergebnisse").click()
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://www.stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000265,-Astartseite")
 
-entries = driver.find_elements_by_class_name("tbdata")
+form = browser.select_form("#cn_loginForm")
+form["usrname"] = account[0]
+form["pass"] = account[1]
+r = browser.submit_selected()
+print(r.text)
+browser.launch_browser()
+
+browser.follow_link(browser.find_link(link_text="Studium"))
+browser.follow_link(browser.find_link(link_text="Prüfungsergebnisse"))
+
+entries = getEntries(browser)
 entrynumber = len(entries)
 
 while True:
 	
-	driver.refresh()
-	entries = driver.find_elements_by_class_name("tbdata")
-	if entrynumber != len(entries):
+	browser.refresh()
+	entries = getEntries(browser)
+
+	if entrynumber < len(entries):
 		handleChange(entries[entrynumber])
 	entrynumber = len(entries)
 	time.sleep(300)
